b7f5989d0be79e82888a02920d2d8ab6f35a3659
John Kearney <john.d.kearney1@gmail.com>
2020-01-14 19:00:30 -0600

    first hw complete

--- homework/exercise1_1-14.py
+++ homework/exercise1_1-14.py
@@ -1,31 +1,171 @@
-# ===============================================
-# Medium
-# ===============================================
-#1 tip calculator
-total_bill = int(input('What was your total bill? Do not include the dollar sign: '))
-level_of_service = input('What was your level of service? Choose good/fair/bad: ')
+# =====================================================================================================
+# Medium Problems
+# =====================================================================================================
+#==================
+# 1 tip calculator
+#==================
+# total_bill = float(input('What was your total bill? Do not include the dollar sign: '))
+# level_of_service = input('What was your level of service? Choose good/fair/bad: ')
 
-if level_of_service == 'good':
-    tip_amount = total_bill * .20
-    total_amount = total_bill * 1.20
-    print(f'Tip Amount: ${tip_amount}')
-    print(f'Total Amount: ${total_amount}')
-elif level_of_service == 'fair':
-    tip_amount = total_bill * .15
-    total_amount = total_bill * 1.15
-    print(f'Tip Amount: ${tip_amount}')
-    print(f'Total Amount: ${total_amount}')
-elif level_of_service == 'fair':
-    tip_amount = total_bill * .10
-    total_amount = total_bill * 1.10
-    print(f'Tip Amount: ${tip_amount}')
-    print(f'Total Amount: ${total_amount}')
-else:
-    print("Try again, make sure to choose good, fair, or bad.")
 
-print(f'Tip amount: {tip_amount}')
-#test
-print("test")
+# if level_of_service == 'good':
+#     tip_amount = total_bill * .20
+#     total_amount = total_bill * 1.20
+#     print(f'Tip Amount: ${tip_amount:.2f}') # :.2f is the newer version of "%.2f"
+#     print(f'Total Amount: ${total_amount:.2f}')
+# elif level_of_service == 'fair':
+#     tip_amount = total_bill * .15
+#     total_amount = total_bill * 1.15
+#     print(f'Tip Amount: ${tip_amount:.2f}')
+#     print(f'Total Amount: ${total_amount:.2f}')
+# elif level_of_service == 'bad':
+#     tip_amount = total_bill * .10
+#     total_amount = total_bill * 1.10
+#     print(f'Tip Amount: ${tip_amount:.2f}')
+#     print(f'Total Amount: ${total_amount:.2f}')
+# else:
+#     print("Try again, make sure to choose good, fair, or bad.")
+
+#==================
+#2 tip calculator 2
+#==================
+# total_bill = float(input('What was your total bill? Do not include the dollar sign: '))
+# split_num = int(input('How many people will be splitting the check?: ' ))
+# level_of_service = input('What was your level of service? Choose good/fair/bad: ')
+
+
+# if level_of_service == 'good':
+#     tip_amount = total_bill * .20
+#     total_amount = total_bill * 1.20
+#     print(f'Tip Amount: ${tip_amount:.2f}') # :.2f is the newer version of "%.2f"
+#     print(f'Total Amount: ${total_amount:.2f}')
+#     print(f'Amount per person: ${total_amount/split_num:.2f}')
+# elif level_of_service == 'fair':
+#     tip_amount = total_bill * .15
+#     total_amount = total_bill * 1.15
+#     print(f'Tip Amount: ${tip_amount:.2f}')
+#     print(f'Total Amount: ${total_amount:.2f}')
+#     print(f'Amount per person: ${total_amount/split_num:.2f}')
+# elif level_of_service == 'bad':
+#     tip_amount = total_bill * .10
+#     total_amount = total_bill * 1.10
+#     print(f'Tip Amount: ${tip_amount:.2f}')
+#     print(f'Total Amount: ${total_amount:.2f}')
+#     print(f'Amount per person: ${total_amount/split_num:.2f}')
+# else:
+#     print("Try again, make sure to choose good, fair, or bad.")
+
+
+#==================
+#3 how many coins
+#==================
+
+# num_of_coins = 0
+# print(f'You have {num_of_coins} coins.')
+# want = input("Do you want another? (Yes/No): ")
+# want = want.lower()
+
+# while want != 'no':
+#     num_of_coins += 1
+#     print(f'You have {num_of_coins} coins.')
+#     want = input("Do you want another? (yes/no): ")
+
+#==================
+#4 Print a box
+#==================
+# width = int(input('width?: '))
+# height = int(input('Height?: '))
+# top_bottom_row_string = '*' * width
+# middle_row_blanks = ' ' * (width - 2)
+# middle_row = '*' + middle_row_blanks + '*'
+
+# print(top_bottom_row_string)
+# row_counter = 1
+# while row_counter <= height - 2:
+#     row_counter += 1
+#     print(middle_row)
+# print(top_bottom_row_string)
+
+#==================
+#5 Print a Triangle
+#==================
+# print('   *   ')
+# print('  ***  ')
+# print(' ***** ')
+# print('*******')
+
+#======================
+#6 Multiplication Table
+#======================
+# left_num = 1
+# right_num = 1
+
+# while right_num <= 10:
+#         #print(f'{left_num} X {right_num} = ' + str(left_num * right_num))
+#         print(f'{left_num} X {right_num} = ' + str(left_num * right_num))
+#         right_num += 1
+#         if right_num == 11:
+#             right_num = 1
+#             left_num += 1
+#             if left_num == 11:
+#                 break
+
+
+
+# =====================================================================================================
+# Large Problems
+# =====================================================================================================
+
+#======================
+#1 Triangle Numbers
+#======================
+# number_n = 1
+# counter = 1
+# while counter <= 100:
+#     triangle_number = int(number_n*(number_n + 1)/2)
+#     print(f'Triangle Number {counter} is {triangle_number}')
+#     counter += 1
+#     number_n += 1
+
+#======================
+#2 Factor a Number
+#======================
+# num_to_factor = int(input("Find the factors of the following positive integer: "))
+# print(f'The factors of {num_to_factor} are:')
+# count_for_division = 1
+# while count_for_division <= num_to_factor:
+#     even_check = num_to_factor % count_for_division
+#     if even_check == 0:
+#         print(f'{count_for_division}')
+#     count_for_division += 1
+
+#======================
+#3 Guess a Number
+#======================
+import random
+number_to_guess = random.randint(1, 10)
+guess_count = 0
+
+print('I am thinking of a number between 1 and 10.')
+number_guessed = int(input('What\'s the number? You get 5 guesses: '))
+while number_guessed != number_to_guess:
+    guess_count += 1
+    if number_guessed < number_to_guess:
+        print("You guessed too low!")
+    elif number_guessed > number_to_guess:
+        print("You guessed too high!")
+
+    if guess_count == 5:
+        print('You ran out of guesses, noob!')
+        break
+    print(f'You have {5 - guess_count} guesses left.')
+    number_guessed = int(input('What\'s the number?: '))
+    if number_guessed == number_to_guess:
+        print('You win!')
+        break
+
+
+
 
 # ===============================================
 # SMALL
